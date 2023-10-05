050c9267aed6352d0a7184b107e705e11d4ac590
everton_bellmann <everton.bellmann@klab.com.br>
2023-10-05 02:12:28 -0300

    feat: layout ok

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -29,74 +29,101 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    widget.homeController.showList();
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
-        child:
-            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          Text(
-            'PESQUISA',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).primaryColor),
-          ),
-          const SizedBox(
-            height: 36,
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-                contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(16)),
-                )),
-            keyboardType: TextInputType.number,
-            controller: _textEditingController,
-            inputFormatters: [
-              MaskFormatter.cepMaskFormatter,
-            ],
-          ),
-          const SizedBox(
-            height: 36,
-          ),
-          ElevatedButton(
-              onPressed: () {
-                widget.homeController.findCep(_textEditingController.text).then(
-                    (value) =>
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text(value),
-                          backgroundColor: Colors.green[400],
-                        )));
-              },
-              child: const Text('Buscar')),
-          Expanded(
-              flex: 2,
-              child: ListenableBuilder(
-                listenable: widget.homeController.isLoader,
-                builder: (context, child) {
-                  return widget.homeController.isLoader.value
-                      ? const Center(
-                          child: LinearProgressIndicator(
-                          minHeight: 12,
-                        ))
-                      : const SizedBox.shrink();
+        child: SingleChildScrollView(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            Text(
+              'PESQUISA',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColor),
+            ),
+            const SizedBox(
+              height: 36,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                  )),
+              keyboardType: TextInputType.number,
+              controller: _textEditingController,
+              inputFormatters: [
+                MaskFormatter.cepMaskFormatter,
+              ],
+            ),
+            const SizedBox(
+              height: 36,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  widget.homeController
+                      .findCep(_textEditingController.text)
+                      .then((value) =>
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(value),
+                            backgroundColor: Colors.green[400],
+                          )));
                 },
-              )),
-          Expanded(
-              flex: 9,
-              child: ValueListenableBuilder(
-                valueListenable: widget.homeController.listCeps,
-                builder: (BuildContext context, value, Widget? child) {
-                  final list = widget.homeController.listCeps.value;
-                  return ListView.builder(
+                child: const Text('Buscar')),
+            const SizedBox(
+              height: 16,
+            ),
+            ListenableBuilder(
+              listenable: widget.homeController.isLoader,
+              builder: (context, child) {
+                return widget.homeController.isLoader.value
+                    ? const Center(
+                        child: LinearProgressIndicator(
+                        minHeight: 12,
+                      ))
+                    : const SizedBox.shrink();
+              },
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ValueListenableBuilder(
+              valueListenable: widget.homeController.listCeps,
+              builder: (BuildContext context, value, Widget? child) {
+                final list = widget.homeController.listCeps.value;
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  child: ListView.builder(
                     itemCount: list.length,
                     itemBuilder: (context, index) {
                       return Card(
                         child: ListTile(
-                          leading: Text(list[index].localidade ?? ''),
-                          title: Text(list[index].cep),
+                          title: Text(
+                            list[index].localidade,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(list[index].logradouro),
+                          leading: Container(
+                            alignment: Alignment.center,
+                            height: 80,
+                            width: 70,
+                            decoration: const BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(150)),
+                                color: Color.fromARGB(255, 162, 222, 193)),
+                            child: Text(
+                              list[index].cep,
+                              style: const TextStyle(
+                                  fontSize: 10, fontWeight: FontWeight.bold),
+                            ),
+                          ),
                           trailing: IconButton(
                             icon: const Icon(Icons.delete),
                             onPressed: () async {
@@ -107,10 +134,13 @@ class _HomePageState extends State<HomePage> {
                         ),
                       );
                     },
-                  );
-                },
-              ))
-        ]),
+                  ),
+                );
+              },
+            ),
+            SizedBox()
+          ]),
+        ),
       ),
     );
   }
